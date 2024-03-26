bd2add279af9e12f6dd327a8cffad4b1f90e3fc5
DHETTIARACHCHI <614237694>
2024-03-26 17:03:04 +0530

    added search product

--- dramatic-application/src/components/NavBar/NavBar.tsx
+++ dramatic-application/src/components/NavBar/NavBar.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.scss';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart, faHeart } from '@fortawesome/free-solid-svg-icons';
 import { useCart } from '../Cart/CartContext';
-import { getAllNotificationsBySellerEmail } from '../../services/apiService';
+import { getAllNotificationsBySellerEmail, searchByQuery } from '../../services/apiService';
 
 interface DecodedToken {
   sub: string;
@@ -17,6 +17,7 @@ interface DecodedToken {
   username: string;
 }
 
+
 const NavBar = () => {
   const { cartCount } = useCart();
   const navigate = useNavigate();
@@ -27,6 +28,8 @@ const NavBar = () => {
     navigate("/");
     window.location.reload();
   };
+
+  const [showHideSearchBar, setShowHideSearchBar] = useState<boolean>(false);
 
   const decodedTokenString = sessionStorage.getItem('decodedToken');
   const decodedToken: DecodedToken | null = decodedTokenString ? JSON.parse(decodedTokenString) : null;
@@ -46,6 +49,25 @@ const NavBar = () => {
       navigate("/verifyEmail")
     }
   };
+
+  const [searchValue, setSearchValue]= useState<string>("")
+
+  const handleSearchinput = (ev:React.ChangeEvent<HTMLInputElement>)=>{
+    setSearchValue(ev.target.value);
+    
+  }
+
+  const handlesearch = () => {
+    setShowHideSearchBar(!showHideSearchBar);
+
+  }
+
+  const clickSearchHandler =async()=> {
+    setShowHideSearchBar(false);
+    // searchCalled(searchValue)
+    navigate(`/search/${searchValue}`);
+   
+  }
 
   const handleNotificationClick = async () => {
     if (decodedToken && decodedToken.role === 'seller') {
@@ -87,7 +109,8 @@ const NavBar = () => {
           <Link to="/viewWish" className="heart-link">
             <FontAwesomeIcon icon={faHeart} className="heart-icon" style={{ color: 'black' }} />
           </Link>
-          <i className="bi bi-search"></i>
+          <i className="bi bi-search" onClick={handlesearch}></i>
+
           <i className="bi bi-person" onClick={checkVerificationStatus}></i>
           <NavLink to="/notifications">
             <i className="bi bi-bell" onClick={handleNotificationClick}></i>
@@ -105,6 +128,18 @@ const NavBar = () => {
           </div>
         </div>
       </div>
+
+      {showHideSearchBar && <div className='search-container'>
+        <div className='search-bar-input-container'>
+          <div className='search'>
+            <form className="d-flex" role="search" >
+              <input onChange={handleSearchinput} className="form-control" type="search" placeholder="Search" aria-label="Search" />
+              <button onClick={clickSearchHandler} className="btn btn-outline-success" type="button">Search</button>
+            </form>
+          </div>
+        </div>
+      </div>}
+
     </div>
   );
 }
